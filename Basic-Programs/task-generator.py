70c980e4dab9597fc9a4d8f5f541c315e95172e0
Uman <213151823+Uman-66@users.noreply.github.com>
2025-07-12 19:44:57 +0500

    Added todo Generator

--- Basic-Programs/task-generator.py
+++ Basic-Programs/task-generator.py
@@ -1,27 +1,27 @@
 loop_on = True
-reallist = []
+real= []
 while loop_on:
     do = input("If you want to add a task press a, if wanna see your todo list press e, if you did something and wanna delete from todo list press d, and if want to exit press x")
 
     if do == "a":
         task = input("What do you want to add?")
-        list.append(task)
+        real.append(task)
 
     elif do == "e":
-        for i in range(len(list)):
-            print(list[i])
+        for i in range(len(real)):
+            print(real[i])
 
     elif do == "x":
         print("Goodbye")
         loop_on = False
 
     elif do == "d":
-        for i in range(len(list)):
-            print(f"{i+1}: {list[i]}")
+        for i in range(len(real)):
+            print(f"{i+1}: {real[i]}")
 
         done = int(input("Which task do you want to delete?, mention its number"))-1
 
-        if done >= 0 and done < len(list):
-            removed = list.pop(done)
+        if done >= 0 and done < len(real):
+            removed = real.pop(done)
             print(f"{removed} was removed")
 
