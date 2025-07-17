5a120e1553040682701cc96df0f50d9d7f03a695
Uman <213151823+Uman-66@users.noreply.github.com>
2025-07-17 23:29:04 +0500

    Added Hangman Game

---
+++ Hangman/main.py
@@ -0,0 +1,39 @@
+from draw import stages
+import random
+word_list = ["ardvark" ,"baboon","camel"]
+lives=5
+random_word=random.choice(word_list)
+
+
+underscore=[]
+for char in random_word:
+    underscore.append("_")
+
+
+end = False
+print(underscore)
+
+while end == False:
+    user=input(f"Guess the word here, you have {lives} guesses left ").lower()
+
+    if user in random_word:
+        for i in range(len(random_word)):
+
+            if random_word[i]==user:
+                underscore[i]=user
+
+
+    else:
+        print(stages[lives])
+        lives -= 1
+    print(underscore)
+
+
+    if lives == 0:
+        print("you lose")
+        end = True
+
+
+    elif not "_" in underscore:
+        end = True
+        print("you win")
